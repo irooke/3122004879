e94ec78cee75e9e33eaa59ac22e991c4768ea7d4
irooke <128659723+irooke@users.noreply.github.com>
2024-03-14 13:58:33 +0800

    Update MainPaperCheck.java

--- src/main/java/MainPaperCheck.java
+++ src/main/java/MainPaperCheck.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class MainPaperCheck {
+import Util.HammingUtils;
+import Util.SimHashUtils;
+import Util.TxtIOUtil;
+
+public class MainPaperCheck {
+
+    public static void main(String[] args) {
+        // 从命令行输入的路径名读取对应的文件，将文件的内容转化为对应的字符串
+        String str0 = TxtIOUtil.readTxt(args[0]);
+        String str1 = TxtIOUtil.readTxt(args[1]);
+        String resultFileName = args[2];
+        // 由字符串得出对应的 simHash值
+        String simHash0 = SimHashUtils.getSimHash(str0);
+        String simHash1 = SimHashUtils.getSimHash(str1);
+        // 由 simHash值求出相似度
+        double similarity = HammingUtils.getSimilarity(simHash0, simHash1);
+        // 把相似度写入最后的结果文件中
+        TxtIOUtil.writeTxt(similarity, resultFileName);
+        // 退出程序
+        System.exit(0);
+    }
+
 }
