af972ca689895d7290e8ba47f962df65371650a9
irooke <ARookieA@outlook.com>
2024-03-14 13:38:05 +0800

    运行

--- src/main/java/Util/ShortStringException.java
+++ src/main/java/Util/ShortStringException.java
@@ -1,3 +1,5 @@
+package Utils;
+
 public class ShortStringException extends Exception {
 
     public ShortStringException() {
--- src/main/java/Util/TxtIOUtil.java
+++ src/main/java/Util/TxtIOUtil.java
@@ -1,3 +1,5 @@
+package Utils;
+
 import java.io.*;
 import java.util.ArrayList;
 
